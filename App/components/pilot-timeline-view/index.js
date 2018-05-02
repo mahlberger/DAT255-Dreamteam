c22d8bad3fc83e34b4119413ff07898f755efb12
fendelin <fendelin@live.se>
2018-05-02 15:49:52 +0200

    Added a list of all favorite portcalls @Anton o Filip

--- App/components/pilot-timeline-view/index.js
+++ App/components/pilot-timeline-view/index.js
@@ -1,24 +1,56 @@
 
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import {
-  View,
-  Modal,
+    View,
+    Text,
+    StyleSheet,
+    ScrollView,
+    RefreshControl,
+    Alert,
+    Modal,
 } from 'react-native';
+
+import {
+    SearchBar,
+    Button,
+    List,
+    ListItem,
+    Icon,
+} from 'react-native-elements';
+
+import {
+    updatePortCalls,
+    selectPortCall,
+    toggleFavoritePortCall,
+    toggleFavoriteVessel,
+    appendPortCalls,
+    bufferPortCalls,
+    setError,
+ } from '../../actions';
+
 
 import TopHeader from '../top-header-view';
 import colorScheme from '../../config/colors';
 
 
-export default class PilotTimeLineView extends Component {
+class PilotTimeLineView extends Component {
   constructor(props) {
       super(props);
-      this.state = {showChangeLog: false}
+      const {portCalls} = this.props;
+      var text = "Hello World2!";
+      var text2;
+      this.state = { numLoadedPortCalls: 20,};
+      
+      this.state = {showChangeLog: false, titleText: "Hejsan", hw: text2};
+
   }
 
   render() {
+
+  const {portCalls} = this.props;
+  var listOfFavoritePortcalls = this.props.favoritePortCalls; //An array of all favoritedPortcalls. 
     return(
       <View>
         <Modal
@@ -31,15 +63,47 @@ export default class PilotTimeLineView extends Component {
             <TopHeader modal title="Change log" backArrowFunction={() => this.setState({showChangeLog: false})}/>
             
         </Modal>
+
         <TopHeader
           title="Pilot Scheduling"
           firstPage
           navigation={this.props.navigation}
         />
+
+        <View>
+          <View>
+          <Text> {listOfFavoritePortcalls.toString()} 
+      
+          </Text>
+
+          </View>
         
+        </View>
       </View>
     );
   }
 }
 
+function mapStateToProps(state) {
+    return {
+        portCalls: state.cache.portCalls,
+        cacheLimit: state.cache.limit,
+        favoritePortCalls: state.favorites.portCalls,
+        favoriteVessels: state.favorites.vessels,
+        showLoadingIcon: state.portCalls.portCallsAreLoading,
+        filters: state.filters,
+        error: state.error,
+        isAppendingPortCalls: state.cache.appendingPortCalls
+    }
+}
+
+export default connect(mapStateToProps, {
+    updatePortCalls,
+    appendPortCalls,
+    selectPortCall,
+    toggleFavoritePortCall,
+    toggleFavoriteVessel,
+    bufferPortCalls,
+    setError,
+})(PilotTimeLineView);
 
