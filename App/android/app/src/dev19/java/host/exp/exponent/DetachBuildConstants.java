d620d0df5ada8a843c51b63cfd01072443d4b54f
mahlberger <martinahlberger@MacBook-Pro-15.local>
2018-05-05 18:40:14 +0200

    Rebuilded code, moved functions and added buttons for zoom in and out

--- App/android/app/src/dev19/java/host/exp/exponent/DetachBuildConstants.java
+++ App/android/app/src/dev19/java/host/exp/exponent/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-    public static final String DEVELOPMENT_URL = "exp432e0186c47243658eeef37602b6c1c4://10.0.124.114:19000";
+    public static final String DEVELOPMENT_URL = "exp432e0186c47243658eeef37602b6c1c4://192.168.0.195:19000";
 
 }
--- App/components/pilot-timeline-view/index.js
+++ App/components/pilot-timeline-view/index.js
@@ -24,53 +24,17 @@ import TopHeader from '../top-header-view';
 import { APP_VERSION } from '../../config/version';
 import colorScheme from '../../config/colors';
 
-const colWidth = 60;
-const HoursLookingBack = 48;
 const portcallIndex = 0;
 
 export default class PilotTimeLineView extends Component {
   constructor(props) {
       super(props);
-      this.state = {showChangeLog: false};
+      this.state = {showChangeLog: false, colWidth: 60, hoursLookingBack: 10, hoursLookingForward: 10 };  
 
-	  this.rows = [];
-	  for(var i = 1; i < 4;  i++){
-		  this.rows.push('');
-	  }
-
-    this.cols = [];
-    for(var j = 0; j < 24*4; j++){
-      this.cols.push('');
-    }
-
-    this.portCalls = [];
-    for(var j = 0; j < 2; j++){
-      this.portCalls.push(new Date());
-    }
-
-    // Test för datum
-    var testDate = new Date();
-    testDate.setHours(testDate.getHours()+2);
-    this.portCalls.push(testDate);
+    this.updateZoomState = this.updateZoomState.bind(this);
 
     this.now = new Date();
-    
-    this.firstTime = new Date(Math.floor(this.now.getTime()/1000/60/60)*1000*60*60);
-    this.firstTime.setHours(this.firstTime.getHours()-HoursLookingBack); 
-    
-    this.lineCurrentTimeOffSet = ((this.now-this.firstTime)/(1000*60*60))*(colWidth); 
-    console.log(colWidth);
-    
-    console.log("Firstime: " + this.firstTime.getTime());
 
-    this.cols = [];
-    for( var j = 0; j < 96; j++){
-      
-      currentTime = new Date();
-      currentTime.setTime(this.firstTime.getTime() + 1000*60*60*j); //add one hour
-      console.log("Firstime: " + currentTime.getTime());
-      this.cols.push({key: j, timeObj: currentTime});
-    }
   }
 
   intToTimeString(timeObj) {
@@ -88,21 +52,58 @@ export default class PilotTimeLineView extends Component {
   componentDidMount() {
     console.log(78);
     const fx = () => this.refs._scrollViewHorizontal.scrollTo({
-      x: this.lineCurrentTimeOffSet - Dimensions.get('window').width/2,
+      x: this.getLeftOffSet(this.now) - Dimensions.get('window').width/2,
       animated: false
     });
     timerid = setTimeout( fx, 30 );
   }
 
-  getLeftOffSet(date){
-      var offset;
-      offset = ((date-this.firstTime)/(1000*60*60))*(colWidth);
-      console.log(offset);
-      return offset;
+  getLeftOffSet(date, string){
+    var offset;
+    offset = ((date-this.firstTime)/(1000*60*60))*(this.state.colWidth);
+    return offset;
+  }
+
+  updateZoomState(value) {
+    console.log(value);
+    this.setState(prevState => {
+       return {colWidth: prevState.colWidth + value}
+    });
+    if (value>0) {
+      console.log("Forward"); 
+      this.setState(prevState => {
+       return {hoursLookingForward: prevState.hoursLookingForward + 5}
+      });
+    }
+    else {
+      this.setState(prevState => {
+       return {hoursLookingBack: prevState.hoursLookingBack + 5}
+      });
+    }    
   }
 
   render() {
     const BULLET = '\u2022';
+    this.firstTime = new Date(Math.floor(this.now.getTime()/1000/60/60)*1000*60*60);
+    this.firstTime.setHours(this.firstTime.getHours()-this.state.hoursLookingBack); 
+
+    this.cols = [];
+    for( var j = 0; j < this.state.hoursLookingBack + this.state.hoursLookingForward; j++){
+      currentTime = new Date();
+      currentTime.setTime(this.firstTime.getTime() + 1000*60*60*j); //add one hour
+      this.cols.push({key: j, timeObj: currentTime});
+    }
+
+    this.portCalls = [];
+    for(var j = 0; j < 2; j++){
+      this.portCalls.push(new Date());
+    }
+
+    // Test för ny tid
+    var testDate = new Date();
+    testDate.setHours(testDate.getHours()+2);
+    this.portCalls.push(testDate);
+
     return(
       <View>
         <Modal
@@ -120,67 +121,58 @@ export default class PilotTimeLineView extends Component {
           firstPage
           navigation={this.props.navigation}
         />
-        <ScrollView>
-			<ScrollView ref='_scrollViewHorizontal' horizontal={true}> 
-				<Grid style={styles.container}>
-					<View style={
-						{
-						   borderRightColor: 'red',
-						   borderRightWidth: 2,
-						   position: 'absolute',
-						   top: 0,
-						   left: this.lineCurrentTimeOffSet,
-						   width: 0,
-						   height: '100%'
-						}
-					}>
-					</View>
-            <Row key = { 0 } style = { styles.row }>
-              {
-                this.cols.map((itemCol, keyCol) =>
-                (
 
-                  <Col key = { keyCol } style = { styles.col2}>
-                    <Text style = {styles.colText}>{this.intToDateString(itemCol.timeObj)}{'\n'}{this.intToTimeString(itemCol.timeObj)}</Text>  
-
-
-                  </Col>
-                ))
-              }
-            </Row>
+			<ScrollView ref='_scrollViewHorizontal' horizontal={true} style= {{height: 300}}> 
+				<View style={
 					{
-					  this.rows.map(( itemRow, keyRow ) =>
-					  (
-						<Row key = { keyRow } style = { styles.row }>
-							{
-								this.cols.map((itemCol, keyCol) =>
-								(
-									<Col key = { keyCol } style = { styles.col}>
-									</Col>
-								))
-							}
-						</Row>
-					  ))
+					   borderRightColor: 'red',
+					   borderRightWidth: 2,
+					   position: 'absolute',
+					   top: 0,
+					   left: this.getLeftOffSet(this.now), 
+					   width: 0,
+					   height: '100%'
 					}
-          {
-            this.portCalls.map((item, key) =>
-            (
-              <View key = { key } style = { {
-                width: 20,
-                height: 20,
-                borderColor: 'blue',
-                borderWidth: 2,
-                position: 'absolute',
-                left: this.getLeftOffSet(item.getTime()),
-                top: portcallIndex++*40 
+				}>
+				</View>
+        {
+          this.cols.map((itemCol, keyCol) =>
+          (
+            <View key = { keyCol } style = {[ styles.timelineHeader, {left: -this.state.colWidth/2, width: this.state.colWidth }]}>  
+              <Text style = {styles.colText}>{this.intToDateString(itemCol.timeObj)}{'\n'}{this.intToTimeString(itemCol.timeObj)}</Text>  
+            </View>
+          ))
+        }
+				{
 
-              }}>
-              </View>
-            ))
-          }
-				</Grid>
-			</ScrollView>
+					this.cols.map((itemCol, keyCol) =>
+					(
+						<View key = { keyCol } style = {[ styles.timelineRuler, {left: this.getLeftOffSet(itemCol.timeObj)}]}> 
+              
+            </View>
+					))
+				}
+        {
+          this.portCalls.map((item, key) =>
+          (
+            <View key = { key } style = { [ styles.timelinePortcall, {left: this.getLeftOffSet(item.getTime()), top: portcallIndex++*40 }]}>
+            </View>
+          ))
+        }
 		</ScrollView>
+    <Button
+      onPress={ () => this.updateZoomState(10)}
+      title="Zoom out"
+      color="#841584"
+      accessibilityLabel="zoom out"
+    />
+
+      <Button
+      onPress={ () => this.updateZoomState(-10)}
+      title="Zoom in"
+      color="#841584"
+      accessibilityLabel="zoom in"
+    />
 
       </View>
     );
@@ -191,8 +183,25 @@ const styles = StyleSheet.create({
     row: {
       height: 200
     },
+    timelineHeader: {
+      height: 50, 
+    },
+    timelineRuler: {
+      width: 1,
+      height: 300,
+      backgroundColor: 'black',
+      position: 'absolute',
+      top: 50
+    },
+    timelinePortcall: {
+      width: 30,
+      height: 30,
+      borderColor: 'blue',
+      borderWidth: 2,
+      position: 'absolute',
+      top: 50
+    },
     col: {
-		  width: colWidth,
       borderRightColor: 'black',
       borderRightWidth: 1
     },
