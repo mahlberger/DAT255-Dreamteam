7409f40a24f44a0e552faffe79025d6c13d55728
Mattias Eriksson <mattieri@student.chalmers.se>
2018-05-07 14:23:42 +0200

    Add_easy-grid-view

--- App/components/pilot-timeline-view/index.js
+++ App/components/pilot-timeline-view/index.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Col, Row, Grid } from "react-native-easy-grid";
 import moment from 'moment';
 
 import {
