198b293e6cf068167081ec0c8ed35a8049784a6d
Martin Ahlberger <martinahlberger@MacBook-Pro-15.local>
2018-05-16 09:29:23 +0200

    mergeconflict

--- App/android/app/src/dev19/java/host/exp/exponent/DetachBuildConstants.java
+++ App/android/app/src/dev19/java/host/exp/exponent/DetachBuildConstants.java
@@ -3,10 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-<<<<<<< HEAD
-    public static final String DEVELOPMENT_URL = "exp432e0186c47243658eeef37602b6c1c4://10.0.186.114:19000";
-=======
-    public static final String DEVELOPMENT_URL = "exp432e0186c47243658eeef37602b6c1c4://8e-krf.lingefjard.app.exp.direct:80";
->>>>>>> 58e983a32839ffab5b1cb23b69f5a78eaf4c60b7
+    public static final String DEVELOPMENT_URL = "exp432e0186c47243658eeef37602b6c1c4://10.0.114.134:19000";
 
 }
