c08e6ff889cbe964f758fd8a53f8233e33f56d22
Martin Ahlberger <martinahlberger@MacBook-Pro-15.local>
2018-05-23 13:07:14 +0200

    Added circles and bugfix

--- App/android/app/src/dev19/java/host/exp/exponent/DetachBuildConstants.java
+++ App/android/app/src/dev19/java/host/exp/exponent/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-    public static final String DEVELOPMENT_URL = "exp432e0186c47243658eeef37602b6c1c4://10.0.114.134:19000";
+    public static final String DEVELOPMENT_URL = "exp432e0186c47243658eeef37602b6c1c4://172.20.10.3:19000";
 
 }
